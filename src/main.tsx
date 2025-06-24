ce686b795a2e38f832d563d6053002c89c328566
xalatechnologies <info@xala.no>
2025-06-24 05:31:49 +0200

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import App from './src/App.tsx'
 import './index.css'
 
 createRoot(document.getElementById("root")!).render(<App />);
